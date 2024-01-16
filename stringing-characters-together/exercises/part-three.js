c02feee1dc916535df66ce9806ecd7ecf3d33c50
luciemariec <132121627+luciemariec@users.noreply.github.com>
2024-01-16 17:41:00 -0600

    finished

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,11 +3,14 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
+console.log(language.slice(0,1)+language.slice(4,5));
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
+let result = language.charAt(0).toUpperCase() + language.charAt(4).toUpperCase();
+console.log(result);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+console.log(`The abbreviation for '${language}' is '${language[0]}${language[4]}'.`);
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
 //Part Three section Two
@@ -15,3 +18,10 @@ let language = 'JavaScript';
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let titleCase = notTitleCase
+  .toLowerCase()
+  .split(' ')
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(' ');
+
+console.log(titleCase);
--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -3,30 +3,45 @@
 let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 // First, print out the dna strand in it's current state.
+//console.log(dna)
 
 //1) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
-
-console.log(/* Your code here. */);
+//let newString = dna.trim();
+//console.log(newString);
 
 //2) Change all of the letters in the dna string to UPPERCASE, then print the result.
+//let uppercasedDNA = dna.toUpperCase();
+//console.log(uppercasedDNA);
 
-console.log();
 
 //3) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
-
-console.log(dna);
+//dna = dna.trim().toUpperCase();
+//console.log(dna);
 
 //Part Two Section Two
 
 let dnaTwo = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
 
 //1) Replace the gene "GCT" with "AGG", and then print the altered strand.
+dnaTwo = dnaTwo.replace('GCT','AGG');
+console.log(dnaTwo);
 
 //2) Look for the gene "CAT" with ``indexOf()``. If found print, "CAT gene found", otherwise print, "CAT gene NOT found".
+let indexOfCAT = dnaTwo.indexOf("CAT");
+
+if (indexOfCAT !== -1) {
+    console.log("CAT gene found");
+} else {
+    console.log("CAT gene NOT found");
+}
 
 //3) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
+console.log(dnaTwo.slice(16,19));
 
 //4) Use a template literal to print, "The DNA strand is ___ characters long."
+let dnaLength = dnaTwo.length;
+console.log(`The DNA strand is ${dnaLength} characters long.`)
 
 //5) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
+console.log(`${dnaTwo.slice(4,7).toLowerCase()}o ${dnaTwo.slice(dna.indexOf('CAT'),dnaTwo.indexOf('CAT')+3).toLowerCase()}`);
