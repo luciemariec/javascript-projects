1bb2b8d896cba7c5c45539945424cb5a087c4cae
luciemariec <132121627+luciemariec@users.noreply.github.com>
2024-01-27 23:22:29 -0600

    chapter examples complete

--- data-and-variables/chapter-examples/more-on-numbers.js
+++
@@ -1,8 +0,0 @@
-console.log(42000);
-console.log(42,000);
-
-console.log(42, 17, 56, 34, 11, 4.35, 32);
-console.log(3.4, "hello", 45);
-console.log(1 + 5 % 3);
-console.log(2 ** 2 ** 3 * 3)
-console.log(16 - 2 * 5 / 3 + 1)
--- data-and-variables/chapter-examples/readline-sync.js
+++
@@ -1,9 +0,0 @@
-const input = require('readline-sync');
-
-// let info = input.question("Question text... ");
-// const input = require('readline-sync');
-
-let info = input.question("Please enter your age: ");
-//The user enters 25.
-
-console.log(typeof info);
