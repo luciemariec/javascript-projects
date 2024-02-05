064eb021f14149cc8b73b58fdfc56f19446e8465
luciemariec <132121627+luciemariec@users.noreply.github.com>
2024-02-04 22:55:15 -0600

    exercises finished

---
+++ modules/exercises/randomSelect.js
@@ -0,0 +1,9 @@
+function randomFromArray(arr){
+  let index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+}
+
+//TODO: Export the randomFromArray function.
+module.exports = {
+  randomFromArray
+};
